06f0dbf6091cc4fafff8ce08efbf7ebfe8c8aebb
Cahyana Cepi <cahyanacepi@gmail.com>
2021-02-17 14:44:34 +0700

    ip

--- application/views/temp/menu.php
+++ application/views/temp/menu.php
@@ -10,8 +10,8 @@
                     
                     <li><a title="Verifikasi" href="<?php echo base_url();?>dashboard" class="menuclick iq-waves-effect"><i class="ri-chat-check-fill"></i><span>Verifikasi</span></a></li>
                      <hr>
-                        <li><a href="{{url('saran')}}" class="iq-waves-effect"><i class="ri-mail-send-fill"></i><span>Kiriman informasi</span></a></li>
-                        <li><a href="{{url('saran')}}" class="iq-waves-effect"><i class="ri-mail-send-fill"></i><span>Kritik dan saran</span></a></li>
+                        <li><a href="kirim_info" class="iq-waves-effect"><i class="ri-mail-send-fill"></i><span>Kiriman informasi</span></a></li>
+                        <li><a href="saran" class="iq-waves-effect"><i class="ri-mail-send-fill"></i><span>Kritik dan saran</span></a></li>
        <hr>
                    <!--  <li class="iq-menu-title"><i class="ri-subtract-line"></i><span>. . . . . . .</span></li>-->
                      <li  class="ref active main-active" id="referensi">
@@ -19,12 +19,12 @@
 						<i class="ri-apps-fill"></i><span>Pengaturan</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
                         <ul id="ui-elements" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                            <li><a class="menuclick" href="<?php echo base_url()?>data_tahun" ref="referensi" title="Data tahun lulus"> > Tahun lulusan</a></li>
-                           <li><a class="menuclick" href="<?php echo base_url()?>data_tahun" ref="referensi"  title="Data kelas"> > Data kelas</a></li>
-                           <li><a href="<?php echo base_url()?>referensi_tahun"> > Jenjang pendidikan</a></li>
-                           <li><a href="<?php echo base_url()?>referensi_tahun"> > Profesi</a></li>
-                           <li><a href="<?php echo base_url()?>referensi_tahun"> > Penghasilan</a></li>
-                           <li><a href="<?php echo base_url()?>referensi_tahun"> > Golongan darah</a></li> 
-                           <li><a href="<?php echo base_url()?>referensi_tahun"> > Halaman registrasi</a></li> 
+                           <li><a class="menuclick" href="<?php echo base_url()?>data_kelas" ref="referensi"  title="Data kelas"> > Data kelas</a></li>
+                           <li><a href="<?php echo base_url()?>jp"> > Jenjang pendidikan</a></li>
+                           <li><a href="<?php echo base_url()?>profesi"> > Profesi</a></li>
+                           <li><a href="<?php echo base_url()?>penghasilan"> > Penghasilan</a></li>
+                           <li><a href="<?php echo base_url()?>goldar"> > Golongan darah</a></li> 
+                           <li><a href="<?php echo base_url()?>hal_reg"> > Halaman registrasi</a></li> 
                         </ul>
                      </li>
 					             <li class="iq-menu-title"><i class="ri-subtract-line"></i><span> </span></li> 
